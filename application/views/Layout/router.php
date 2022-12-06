d6ecd999cc4f6d28db6f2232d9208da1f37e0f34
egooktafanda97 <egooktafanda1097@gmail.com>
2022-12-07 01:41:46 +0700

    eee

--- application/views/Layout/router.php
+++ application/views/Layout/router.php
@@ -17,36 +17,36 @@
         <ul class="sidebar-menu">
             <li class="menu-header">Dashboard</li>
             <li class="<?= $this->uri->segment(1) == "Dashboard" ? "active" : "" ?>">
-                <a href="<?= base_url('Dashboard/index') ?>"><i class="ion ion-speedometer"></i><span>Dashboard</span></a>
+                <a href="<?= base_url('Dashboard/index') ?>"><i class="ion ion-speedometer"></i><span>DAHBOARD</span></a>
             </li>
 
             <li class="menu-header">Menu</li>
             <li class="active">
-                <a href="#" class="has-dropdown"><i class="ion ion-ios-albums-outline"></i><span>Jurupungut</span></a>
+                <a href="#" class="has-dropdown"><i class="ion ion-ios-albums-outline"></i><span><strong>JURUPUNGUT</strong></span></a>
                 <ul class="menu-dropdown">
-                    <li class="<?= $this->uri->segment(2) == "index" ? "active" : "" ?>"><a href="<?= base_url('Jurupungut/index') ?>"><i class=" ion ion-ios-circle-outline"></i>Juru Pungut</a></li>
-                    <li class="<?= $this->uri->segment(2) == "laporan" ? "active" : "" ?>"><a href="<?= base_url('Jurupungut/laporan') ?>"><i class=" ion ion-ios-circle-outline"></i>Laporan Jurupungut</a></li>
+                    <li class="<?= $this->uri->segment(1) == "Jurupungut" && $this->uri->segment(2) == "index" ? "active" : "" ?>"><a href="<?= base_url('Jurupungut/index') ?>"><i class=" ion ion-ios-circle-outline"></i>JURUPUNGUT</a></li>
+                    <li class="<?= $this->uri->segment(1) == "Jurupungut" && $this->uri->segment(2) == "laporan" ? "active" : "" ?>"><a href="<?= base_url('Jurupungut/laporan') ?>"><i class=" ion ion-ios-circle-outline"></i>LAPORAN JURUPUNGUT</a></li>
                 </ul>
             </li>
             <li class="active">
-                <a href="#" class="has-dropdown"><i class="ion ion-ios-albums-outline"></i><span>Usaha</span></a>
+                <a href="#" class="has-dropdown"><i class="ion ion-ios-albums-outline"></i><span><strong>USAHA</strong></span></a>
                 <ul class="menu-dropdown">
-                    <li class="<?= $this->uri->segment(1) == "Usaha" && $this->uri->segment(2) == "index" ? "active" : "" ?>"><a href="<?= base_url('Usaha/index') ?>"><i class=" ion ion-ios-circle-outline"></i>Data Usaha</a></li>
-                    <li class="<?= $this->uri->segment(1) == "TypeUsaha" && $this->uri->segment(2) == "index" ? "active" : "" ?>"><a href="<?= base_url('TypeUsaha/index') ?>"><i class=" ion ion-ios-circle-outline"></i>Tipe Usaha</a></li>
-                    <li class="<?= $this->uri->segment(1) == "Zona" && $this->uri->segment(2) == "index" ? "active" : "" ?>"><a href="<?= base_url('Zona/index') ?>"><i class="ion ion-ios-circle-outline"></i>Wilayah</a></li>
+                    <li class="<?= $this->uri->segment(1) == "Usaha" && $this->uri->segment(2) == "index" ? "active" : "" ?>"><a href="<?= base_url('Usaha/index') ?>"><i class=" ion ion-ios-circle-outline"></i>DATA USAHA</a></li>
+                    <li class="<?= $this->uri->segment(1) == "TypeUsaha" && $this->uri->segment(2) == "index" ? "active" : "" ?>"><a href="<?= base_url('TypeUsaha/index') ?>"><i class=" ion ion-ios-circle-outline"></i>TIPE USAHA</a></li>
+                    <li class="<?= $this->uri->segment(1) == "Zona" && $this->uri->segment(2) == "index" ? "active" : "" ?>"><a href="<?= base_url('Zona/index') ?>"><i class="ion ion-ios-circle-outline"></i>WILAYAH</a></li>
                 </ul>
             </li>
             <li class="active">
-                <a href="#" class="has-dropdown"><i class="ion ion-ios-albums-outline"></i><span>Retribusi</span></a>
+                <a href="#" class="has-dropdown"><i class="ion ion-ios-albums-outline"></i><span><strong>RETRIBUSI</strong></span></a>
                 <ul class="menu-dropdown">
-                    <li class="<?= $this->uri->segment(1) == "Retribusi" && $this->uri->segment(2) == "index" ? "active" : "" ?>"><a href="<?= base_url('Retribusi/index') ?>"><i class="ion ion-ios-circle-outline"></i>Laporan Retribusi</a></li>
+                    <li class="<?= $this->uri->segment(1) == "Retribusi" && $this->uri->segment(2) == "index" ? "active" : "" ?>"><a href="<?= base_url('Retribusi/index') ?>"><i class="ion ion-ios-circle-outline"></i>LAPORAN RETRIBUSI</a></li>
                     <li class="<?= $this->uri->segment(1) == "RetribusiNpwrd" && $this->uri->segment(2) == "index" ? "active" : "" ?>"><a href="<?= base_url('RetribusiNpwrd/index') ?>"><i class="ion ion-ios-circle-outline"></i>NPWRD</a></li>
                     <!-- <li><a href="components.html"><i class="ion ion-ios-circle-outline"></i> Informasi Peta</a></li> -->
                     <!-- <li class="<?= $this->uri->segment(1) == "Zona" && $this->uri->segment(2) == "index" ? "active" : "" ?>"><a href="<?= base_url('Zona/index') ?>"><i class="ion ion-ios-circle-outline"></i>Wilayah</a></li> -->
                 </ul>
             </li>
             <li class="active">
-                <a href="#" class="has-dropdown"><i class="ion ion-ios-albums-outline"></i><span>Timbangan</span></a>
+                <a href="#" class="has-dropdown"><i class="ion ion-ios-albums-outline"></i><span><strong>TIMBANGAN TPA</strong></span></a>
                 <ul class="menu-dropdown">
                     <li class="<?= $this->uri->segment(1) == "Timbangan" && $this->uri->segment(2) == "scales" ? "active" : "" ?>"><a href="<?= base_url('Timbangan/scales') ?>"><i class="ion ion-ios-circle-outline"></i>Timbangan</a></li>
                     <!-- <li><a href="components.html"><i class="ion ion-ios-circle-outline"></i>Laporan Timbangan</a></li> -->
